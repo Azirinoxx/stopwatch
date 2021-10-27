a4bd475d9d07a4e68c3bb6524ed9ba1bb7461569
Azirinoxx <93145316+Azirinoxx@users.noreply.github.com>
2021-10-27 14:37:09 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+let start = 0
+let elapsed = 0
+input.onButtonPressed(Button.A, function () {
+    start = input.runningTime()
+})
+input.onButtonPressed(Button.B, function () {
+    elapsed = input.runningTime() - start
+})
